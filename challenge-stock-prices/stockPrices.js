de59982496d6f52eca98ae193a3210697d24c8c4
Damien Redd <damiens.redd@gmail.com>
2023-02-07 09:30:59 -0600

    not working

---
+++ challenge-stock-prices/stockPrices.js
@@ -0,0 +1,23 @@
+let price = [15, 10, 20, 22, 1, 9];
+
+
+function best(arr) {
+if (max <= min)
+return 0;
+
+let profit = 0;
+
+let min = Math.min(...arr)
+let max = Math.max(...arr)
+if ( max - min > 0){
+return profit  
+}
+}
+
+    
+
+
+
+
+
+console.log(best(price))
